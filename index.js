c2ac8c4fd765b7d42fe04e1209bd9e8e3650d40a
Soichiro-Yoshimura <yoshimura@soichiro>
2016-03-09 21:17:26 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function getA() {
+  return new Promise((resolve) => {
+    setTimeout(() => { resolve(11); }, 1000);
+  });
+}
+
+function getB() {
+  return new Promise((resolve) => {
+    setTimeout(() => { resolve(13); }, 1000);
+  });
+}
+
+function getC() {
+  return new Promise((resolve) => {
+    setTimeout(() => { resolve(17); }, 1000);
+  });
+}
+
+// TODO ここに getA, getB, getC で得られる結果をかけあわせた結果 2431 を標準出力するコードを記述する
+// ただし Promise チェイン(then関数の結果に対するthen関数の呼び出し)を一度は用いて実装をすること
