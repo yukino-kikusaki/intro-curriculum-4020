34484d554592619d238837f321a6c612160bce28
yukino-kikusaki <yukino.kikusaki@azone-japan.co.jp>
2024-04-25 14:41:41 +0900

    解答済み

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+console.log('テスト1');
+
 function getA() {
   return new Promise((resolve) => {
     setTimeout(() => { resolve(11); }, 1000);
@@ -20,3 +22,20 @@ function getC() {
 
 // TODO ここに getA, getB, getC で得られる結果をかけあわせた結果 2431 を標準出力するコードを記述する
 // ただし Promise チェイン(then関数の結果に対するthen関数の呼び出し)を一度は用いて実装をすること
+
+getA()
+  .then(a => {
+    return getB()
+      .then(b => {
+        return a * b;
+      });
+    }
+  )
+  .then(result => {
+    getC()
+      .then(c => {
+        console.log(result * c);
+    });
+  });
+
+console.log('テスト2');
